c2edc293e541f5d54bd37fcf4032fe86a2c2f43c
Matheus H. Gois <matheus.hengo@gmail.com>
2021-11-03 19:59:39 -0300

    FEAT<Services>: Add create service

--- backend/src/services/userServices.js
+++ backend/src/services/userServices.js
@@ -1,2 +1,23 @@
 const Users = require('../models/Users');
+const {
+  validateEmail,
+  validatePassword,
+  validateName,
+} = require('../validation/userValidation');
 
+const createUser = (userData) => {
+  const { username, password, email } = userData;
+
+  const invalidEmail = validateEmail(email);
+  if (invalidEmail.error) return invalidEmail;
+
+  const invalidPassword = validatePassword(password);
+  if (invalidPassword.error) return invalidPassword;
+
+  const invalidName = validateName(username);
+  if(invalidName.error) return invalidName;
+};
+
+module.exports = {
+  createUser,
+}
