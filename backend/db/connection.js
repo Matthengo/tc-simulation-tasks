4c5aa914376fd5f57456a8aa482163a0ae2ac9b7
Matheus H. Gois <matheus.hengo@gmail.com>
2021-11-03 17:58:02 -0300

    FIX: Connection to Atlas now is working

--- backend/db/connection.js
+++ backend/db/connection.js
@@ -5,9 +5,8 @@ const OPTIONS = {
     useNewUrlParser: true,
     useUnifiedTopology: true,
 };
-
-const MONGO_DB_URL = process.env.ATLAS_URI || 'mongodb://mongodb:27017/TC-tasks';
-const DB_NAME = 'TC-tasks';
+const MONGO_DB_URL = process.env.ATLAS_URI || 'mongodb://mongodb:27017/TCtasks';
+const DB_NAME = 'TCtasks';
 
 let db = null;
 
@@ -16,6 +15,7 @@ const connection = () => (db
     : MongoClient.connect(MONGO_DB_URL, OPTIONS)
     .then((conn) => {
     db = conn.db(DB_NAME);
+    console.log('Connected to Atlas DB');
     return db;
     }));
 
