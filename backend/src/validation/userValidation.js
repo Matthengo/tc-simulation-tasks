6dbc9038051d293199c9a7caee2af6b45110b90c
Matheus H. Gois <matheus.hengo@gmail.com>
2021-11-03 19:51:45 -0300

    REFACTOR: "validation" to "validate"

--- backend/src/validation/userValidation.js
+++ backend/src/validation/userValidation.js
@@ -1,21 +1,21 @@
 const { dataIsRequired, dataLengthMustBe } = require('../helpers/errorFunctions')
 const { EMAIL_MUST_VALID } = require('../helpers/errorObjects')
 
-const validatingEmail = (email) => {
+const validateEmail = (email) => {
   if (!email) return dataIsRequired('email')
 
   const emailRegex = new RegExp(/^[\w.]+@[a-z]+.\w{2,3}$/g);
   if (!emailRegex.test(email)) return EMAIL_MUST_VALID;
 };
 
-const validatingPassword = (password) => {
+const validatePassword = (password) => {
   if (!password) return dataIsRequired('password');
   
   const PASSWORD_LENGTH = 6;
   if (password.length < PASSWORD_LENGTH) return dataLengthMustBe('password', PASSWORD_LENGTH);
 }
 
-const validatingName = (name) => {
+const validateName = (name) => {
   if(!name) return dataIsRequired('name');
 
   const NAME_LENGTH = 3;
@@ -23,7 +23,7 @@ const validatingName = (name) => {
 }
 
 module.exports = {
-  validatingEmail,
-  validatingName,
-  validatingPassword,
+  validateEmail,
+  validateName,
+  validatePassword,
 }
