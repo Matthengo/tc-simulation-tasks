048969fd2da15dee8ee1e91438a9595dc20a139f
Matheus H. Gois <matheus.hengo@gmail.com>
2021-11-04 19:25:02 -0300

    FIX: Change logic to make the code works

--- backend/src/middlewares/checkUserAuthorization.js
+++ backend/src/middlewares/checkUserAuthorization.js
@@ -3,14 +3,14 @@ const { TASK_NOT_FOUND } = require('../helpers/errorObjects');
 
 module.exports = async (req, res, next) => {
   const {
-    user: { id: tokenUserId },
+    user: { _id: tokenUserId },
     params: { id: paramsId },
   } = req;
 
   const foundTask = await Tasks.getTaskById(paramsId);
   if (!foundTask) next(TASK_NOT_FOUND.error);
 
-  if (foundTask.userId !== tokenUserId) {
+  if (!tokenUserId.equals(foundTask.userId)) {
     return res.status(401).json({ message: 'Unauthorized user' });
   }
 
