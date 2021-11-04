35b3be4f4c6d9b120b077d789c8ddaae1f9e4986
Matheus H. Gois <matheus.hengo@gmail.com>
2021-11-04 15:54:08 -0300

    FIX: Add dataIsRequired at login function

--- backend/src/services/loginServices.js
+++ backend/src/services/loginServices.js
@@ -2,9 +2,13 @@ const Users = require('../models/Users')
 
 const { hasCorrectPassword } = require('../validation/loginValidation')
 const { INCORRECT_DATA } = require('../helpers/errorObjects');
+const { dataIsRequired } = require('../helpers/errorFunctions');
 
 const login = async (userData) => {
   const { email, password } = userData;
+
+  if(!email) return dataIsRequired('email');
+  if(!password) return dataIsRequired('password');
 
   const userFound = await Users.getUser(email);
   if (!userFound) return INCORRECT_DATA;
