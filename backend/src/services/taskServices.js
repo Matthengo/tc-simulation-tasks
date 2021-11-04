9bca1f8e745acc37b7a1b2cdf75448fee6024e64
Matheus H. Gois <matheus.hengo@gmail.com>
2021-11-04 18:50:18 -0300

    REFACTOR: Remove task verification

--- backend/src/services/taskServices.js
+++ backend/src/services/taskServices.js
@@ -1,6 +1,5 @@
 const Tasks = require('../models/Tasks');
 const { dataIsRequired } = require('../helpers/errorFunctions');
-const { TASK_NOT_FOUND } = require('../helpers/errorObjects')
 
 const createTask = async (taskData) => {
   const { taskName } = taskData;
@@ -16,8 +15,8 @@ const updateTask = async (taskData) => {
   const { id, taskName } = taskData;
   if(!taskName) return dataIsRequired('taskName');
 
-  const foundTask = await Tasks.getTaskById(id);
-  if(!foundTask) return TASK_NOT_FOUND;
+  // const foundTask = await Tasks.getTaskById(id);
+  // if(!foundTask) return TASK_NOT_FOUND;
 
   await Tasks.updateTask(taskData);
 
