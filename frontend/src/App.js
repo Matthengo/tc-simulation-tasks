e03a17e39f4d79f4fea6aa4c728196e07abe7e95
Matheus H. Gois <matheus.hengo@gmail.com>
2021-11-05 05:48:25 -0300

    FEAT: Setting context

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import { Route, Switch } from 'react-router';
+import TasksProvider from './context/TasksProvider';
 import LoginPage from './pages/LoginPage';
 import RegisterPage from './pages/RegisterPage';
 
 function App() {
   return (
-    <Switch>
-      <Route exact path="/" component={ RegisterPage } />
-      <Route exact path="/login" component={ LoginPage } />
-    </Switch>
+    <TasksProvider>
+      <Switch>
+        <Route exact path="/" component={ RegisterPage } />
+        <Route exact path="/login" component={ LoginPage } />
+      </Switch>
+    </TasksProvider>
   );
 }
 
