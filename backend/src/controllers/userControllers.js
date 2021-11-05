d2225d2aad7b1941e96367debdde72aa539cea28
Matheus H. Gois <matheus.hengo@gmail.com>
2021-11-05 04:28:09 -0300

    FIX: Returning next() due to error

--- backend/src/controllers/userControllers.js
+++ backend/src/controllers/userControllers.js
@@ -3,7 +3,7 @@ const userServices = require('../services/userServices');
 const createUser = async (req, res, next) => {
   try {
     const { error } = await userServices.createUser(req.body);
-    if(error) next(error);
+    if(error) return next(error);
     res.status(201).json({})
   } catch (error) {
     console.log(error);
